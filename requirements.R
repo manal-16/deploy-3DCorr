e5e55a34ddb8242b0abfe587019d3de614189fef
manal-16 <mac_manal@icloud.com>
2025-05-19 09:28:58 +0200

    requirements.R

---
+++ requirements.R
@@ -0,0 +1,6 @@
+install.packages(c(
+  "shiny", "shinyjs", "shinyalert", "shinycssloaders", "shinyWidgets",
+  "bslib", "dplyr", "stringr", "plotly", "Hmisc", "RColorBrewer",
+  "fpc", "dbscan", "MatrixModels", "htmltools", "strawr",
+  "GenomicFeatures", "HiCBricks"
+), repos = "https://cloud.r-project.org")
