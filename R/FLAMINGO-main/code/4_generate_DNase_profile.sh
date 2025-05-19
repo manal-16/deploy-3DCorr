2dcede33319a4ba0b0a30ef267199f408886cac9
manal-16 <mac_manal@icloud.com>
2025-05-19 09:43:50 +0200

    manifest.json

--- R/FLAMINGO-main/code/4_generate_DNase_profile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd $1
-module load bedtools
-for i in *;do
-	echo $i
-	bedtools map -a $i -b $2 -c 4 -o mean > DNase_$i
-done
-
--- requirements.R
+++
@@ -1,6 +0,0 @@
-install.packages(c(
-  "shiny", "shinyjs", "shinyalert", "shinycssloaders", "shinyWidgets",
-  "bslib", "dplyr", "stringr", "plotly", "Hmisc", "RColorBrewer",
-  "fpc", "dbscan", "MatrixModels", "htmltools", "strawr",
-  "GenomicFeatures", "HiCBricks"
-), repos = "https://cloud.r-project.org")
